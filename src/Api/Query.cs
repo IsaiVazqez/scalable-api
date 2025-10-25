05aec9a9725b93446ddad71dd76c9f9bbb526659
IsaiVazqez <Isai>
2025-10-25 13:38:28 -0600

    feat: add new query endpoints for testing CD - GetTimestamp and GetServerInfo

--- src/Api/Query.cs
+++ src/Api/Query.cs
@@ -2,5 +2,9 @@ namespace Api;
 
 public class Query
 {
-    public string Ping() => "pong";
+    public string Ping() => "pong from updated version 🚀";
+    
+    public string GetTimestamp() => DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss UTC");
+    
+    public string GetServerInfo() => $"Server running on {Environment.MachineName} - Version 2.0";
 }
